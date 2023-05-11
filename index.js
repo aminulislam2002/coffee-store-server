b193a067c74ecd02349c88e3b812045dea9de51a
aminulislam2002 <aminulislamemon2002@gmail.com>
2023-05-11 05:03:18 -0700

    store data in the mongodb database

--- index.js
+++ index.js
@@ -9,11 +9,7 @@ const port = process.env.PORT || 5000;
 app.use(express.json());
 app.use(cors());
 
-console.log(process.env.DB_USER);
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.USER_PASS}@cluster0.fsd9z3z.mongodb.net/?retryWrites=true&w=majority`;
-
-console.log(uri);
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -29,8 +25,15 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
+    const coffeeCollection = client.db("coffeeDB").collection("coffee");
 
-    
+    app.post("/coffee", async (req, res) => {
+      const newCoffee = req.body;
+      console.log(newCoffee);
+      const result = await coffeeCollection.insertOne(newCoffee);
+      res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
